bc1266cd41a6903df5844d6bf58bf99345396784
alad2121 <45607789+alad2121@users.noreply.github.com>
2021-10-01 20:56:18 -0400

    Added comments explaining my code

--- New rebuild/Assets/Code/CameraFollow.cs
+++ New rebuild/Assets/Code/CameraFollow.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
+    // Transform playerFollow is used to track the orientation of the player
     private Transform playerFollow;
 
-    public float smoothSpeed = 0.125f;
+    // Determines in a range from [1 to 10] on how smooth the follow camera should be
+    [Range(1,10)]
+    public float smoothFactor = 0.125f;
+
+    // Used to provide an offset from the player's position
+    public Vector3 offset;
+
+    /*
     public final endBackgroundPosition = 200;
     public final beginBackgroundPosition = 1;
-    public Vector3 offset;
+    */
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +27,27 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 temp = transform.position;
+        // Vector3 targetPosition is used as a postion that needs to be meet by the camera's postion
+        Vector3 targetPosition = playerFollow.position + offset;
 
-        temp.x = playerFollow.position.x;
 
-        transform.position = temp;
+        // Vector3 smoothPosition is used to provide a smooth transition from the orginal camera postion to the target postion
+        // Lerp function linearly interpolates between two points(First two parameters): 
+        // The first two parameters (a,b) creates a range. My range is [transform.position to targetPosition]
+        // The last paramter ranges from [0 to 1] and acts as 
+        // a percentage that is used to determine the value from the range of the two parameters: 
 
+        Vector3 smoothPosition = Vector3.Lerp(transform.position,targetPosition,smoothFactor * Time.fixedDeltaTime);
+
+
+        //Sets the current camera position with an updated position(smoothPosition)
+        transform.position = smoothPosition;
+
+        /*
         if(playerFollow.position.x > endBackgroundPosition){
             temp.x = 
             transform.position = temp;
         }
+        */
     }
 }
